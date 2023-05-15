17087672bd0dc5a2dbf64feb4642da8f1e348cbc
Roman <85442818+mardygalimov@users.noreply.github.com>
2023-05-15 20:05:22 +0400

    Change

---
+++ 3/3.5.1.py
@@ -0,0 +1,6 @@
+apples = 41
+box_size = 3
+boxes = apples // box_size
+over_apples = apples % box_size
+print('Всего полных ящиков:', boxes)
+print('Остаток яблок:', over_apples)
---
+++ 3/3.5.2.py
@@ -0,0 +1,3 @@
+address = int(input('Введите число:' ))
+print('Номер дома:', address % 10)
+print('Номер квартиры:', address // 10)
